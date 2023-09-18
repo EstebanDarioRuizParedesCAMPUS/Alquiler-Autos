6115132a32e70e2cbc66cb7d508db50d418859d8
EstebanDarioRuizParedesCAMPUS <edrp.campus15@gmail.com>
2023-09-17 22:24:50 -0500

    Endpoints 10,11,12,13,14,17 Fixed 8

--- controller/endpoints.controller.js
+++ controller/endpoints.controller.js
@@ -1,256 +1,355 @@
 import { ObjectId } from "mongodb";
-import {conection, client} from "../conection/conectionDB.js";
+import { conection, client } from "../conection/conectionDB.js";
 
 const ejercicio2 = async (req, res) => {
-    try {
-        const colection = (await conection()).Clientes
-    const clientes = await colection.find({}).toArray()
-    res.json(clientes)
-    await client.close()
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')
-    }
-}
+  try {
+    const colection = (await conection()).Clientes;
+    const clientes = await colection.find({}).toArray();
+    res.json(clientes);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
+};
 
 const ejercicio3 = async (req, res) => {
-    try {
-        const colection = (await conection()).Automoviles
-        const automoviles = await colection.find({}).toArray()
-        res.json(automoviles)
-        await client.close()  
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')
-    }
+  try {
+    const colection = (await conection()).Automoviles;
+    const automoviles = await colection.find({}).toArray();
+    res.json(automoviles);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio4 = async (req, res) => {
-    try {
-        const colection = (await conection()).Alquileres
-        const alquileres = await colection.find({
-            Estado: true
-        }).toArray()  
-        res.json(alquileres)
-        await client.close()
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')
-    }
+  try {
+    const colection = (await conection()).Alquileres;
+    const alquileres = await colection
+      .find({
+        Estado: true,
+      })
+      .toArray();
+    res.json(alquileres);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio5 = async (req, res) => {
-    try {
-        const colection = (await conection()).Reservas
-        const reservas = await colection.find({
-            Estado: true
-        }).toArray()
-        res.json(reservas)
-        await client.close()
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')
-    }
+  try {
+    const colection = (await conection()).Reservas;
+    const reservas = await colection
+      .find({
+        Estado: true,
+      })
+      .toArray();
+    res.json(reservas);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio6 = async (req, res) => {
-    try {
-        const {id} = req.query
-        const objectId = new ObjectId(id);
-        const colection = (await conection()).Alquileres
-        const alquileres = await colection.find({
-            _id: objectId
-        }).toArray()
-        res.json(alquileres)
-        await client.close()  
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')
-    }
+  try {
+    const { id } = req.query;
+    const objectId = new ObjectId(id);
+    const colection = (await conection()).Alquileres;
+    const alquileres = await colection
+      .find({
+        _id: objectId,
+      })
+      .toArray();
+    res.json(alquileres);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio7 = async (req, res) => {
-    try {
-        const colection = (await conection()).Empleados
-        const empleados = await colection.find({
-            Cargo: 'Vendedor'
-        }).toArray()
-        res.json(empleados)
-        await client.close()
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+    const colection = (await conection()).Empleados;
+    const empleados = await colection
+      .find({
+        Cargo: "Vendedor",
+      })
+      .toArray();
+    res.json(empleados);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio8 = async (req, res) => {
-    try {
-        const colection = (await conection()).SucursalAutomovil
-const result = await colection.aggregate([
-    {
-      $group: {
-        _id: {
-          sucursal: "$Sucursal.Nombre",
-          automovilMarca: "$Automovil.Marca",
-          automovilModelo: "$Automovil.Modelo",
-          automovilAnio: "$Automovil.Anio",
-        },
-      },
-    },
-    {
-      $group: {
-        _id: "$_id.sucursal",
-        AutomovilesDiponibles: {
-          $push: {
-            automovilMarca: "$_id.automovilMarca",
-            automovilModelo: "$_id.automovilModelo",
-            automovilAnio: "$_id.automovilAnio",
+  try {
+    const colection = (await conection()).SucursalAutomovil;
+    const result = await colection
+      .aggregate([
+        {
+          $group: {
+            _id: {
+              sucursal: "$Sucursal.Nombre",
+              automovilMarca: "$Automovil.Marca",
+              automovilModelo: "$Automovil.Modelo",
+              automovilAnio: "$Automovil.Anio",
+            },
           },
         },
-        TotalAutomovilesDisponibles: { $sum: 1 },
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        Sucursal: "$_id",
-        AutomovilesDiponibles: 1,
-        TotalAutomovilesDisponibles: 1,
-      },
-    },
-  ]).toArray();
-        res.json(result)
-        await client.close()
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+        {
+          $group: {
+            _id: "$_id.sucursal",
+            AutomovilesDiponibles: {
+              $push: {
+                automovilMarca: "$_id.automovilMarca",
+                automovilModelo: "$_id.automovilModelo",
+                automovilAnio: "$_id.automovilAnio",
+              },
+            },
+            TotalAutomovilesDisponibles: { $sum: 1 },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            Sucursal: "$_id",
+            TotalAutomovilesDisponibles: 1,
+          },
+        },
+      ])
+      .toArray();
+    res.json(result);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio9 = async (req, res) => {
-    try {
-            const {id} = req.query
-            const objectId = new ObjectId(id);
-            const colection = (await conection()).Alquileres
-            const alquileres = await colection.find({
-                _id: objectId
-            }).project({
-                CostoTotal : 1
-            }).toArray()
-            res.json(alquileres)
-            await client.close()  
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+    const { id } = req.query;
+    const objectId = new ObjectId(id);
+    const colection = (await conection()).Alquileres;
+    const alquileres = await colection
+      .find({
+        _id: objectId,
+      })
+      .project({
+        CostoTotal: 1,
+      })
+      .toArray();
+    res.json(alquileres);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio10 = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+    const { dni } = req.query;
+    const dniNumero = Number(dni);
+    const colection = (await conection()).Clientes;
+    const clientes = await colection
+      .find({
+        DNI: dniNumero,
+      })
+      .project({
+        _id: 0,
+        DNI: 1,
+        Nombre: 1,
+        Apellido: 1,
+      })
+      .toArray();
+    res.json(clientes);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio11 = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+    const { capacity } = req.query;
+    const capacityNumbe = Number(capacity);
+    const colection = (await conection()).Automoviles;
+    const automoviles = await colection
+      .find({
+        capacidad: { $gt: capacityNumbe },
+      })
+      .toArray();
+    res.json(automoviles);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio12 = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+    const { date } = req.query;
+    const fecha = new Date(date);
+    console.log(date);
+    console.log(fecha);
+    const colection = (await conection()).Alquileres;
+    const alquileres = await colection
+      .find({
+        FechaInicio: fecha,
+      })
+      .toArray();
+    res.json(alquileres);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio13 = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+    const { name, lstName } = req.query;
+    const colection = (await conection()).Reservas;
+    const reservas = await colection
+      .find({
+        "Cliente.Nombre": name,
+        "Cliente.Apellido": lstName,
+      })
+      .toArray();
+    res.json(reservas);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio14 = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+    const { cargo } = req.query;
+    const cargoSeparados = cargo.split("-");
+    const colection = (await conection()).Empleados;
+    const alquileres = await colection
+      .find({
+        Cargo: { $in: cargoSeparados },
+      })
+      .toArray();
+    res.json(alquileres);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio15 = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio16 = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio17 = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+    const colection = (await conection()).SucursalAutomovil;
+    const result = await colection
+      .aggregate([
+        {
+          $group: {
+            _id: {
+              sucursal: "$Sucursal.Nombre",
+              Direccion: "$Sucursal.Direccion",
+              automovilMarca: "$Automovil.Marca",
+              automovilModelo: "$Automovil.Modelo",
+              automovilAnio: "$Automovil.Anio",
+            },
+          },
+        },
+        {
+          $group: {
+            _id: "$_id.sucursal",
+            Direccion: { $first: "$_id.Direccion" },
+            AutomovilesDiponibles: {
+              $push: {
+                automovilMarca: "$_id.automovilMarca",
+                automovilModelo: "$_id.automovilModelo",
+                automovilAnio: "$_id.automovilAnio",
+              },
+            },
+            TotalAutomovilesDisponibles: { $sum: 1 },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            Sucursal: "$_id",
+            Direccion: 1,
+            AutomovilesDiponibles: 1,
+            TotalAutomovilesDisponibles: 1,
+          },
+        },
+      ])
+      .toArray();
+    res.json(result);
+    await client.close();
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio18 = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio19 = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio20 = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 const ejercicio21 = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        console.log(error);
-        res.status(404).end('Dato Inválido/No enontrado')  
-    }
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(404).end("Dato Inválido/No enontrado");
+  }
 };
 
 export {
--- readme.md
+++ readme.md
@@ -21,7 +21,7 @@ http://localhost:9000/API/ejercicio5
 **6. Obtener los detalles del alquiler con el ID_Alquiler específico.**
 
 _El id es solo un ejemplo, puede ser cualquier otro, los ids a utilizar estan en la carppeta `infobase_DB` especificamente en el archivo `tallerAutos.Alquiler.json`_
-http://localhost:9000/API/ejercicio6/?id=_6507549cedc519034cd40bd2_
+http://localhost:9000/API/ejercicio6/?id=6507549cedc519034cd40bd2
 
 **7. Listar los empleados con el cargo de "Vendedor"**
 
@@ -34,39 +34,44 @@ http://localhost:9000/API/ejercicio8
 **9. Obtener el costo total de un al quiler específico.**
 
 _El id es solo un ejemplo, puede ser cualquier otro, los ids a utilizar estan en la carppeta `infobase_DB` especificamente en el archivo `tallerAutos.Alquiler.json`_
-http://localhost:9000/API/ejercicio9/?id=_6507549cedc519034cd40bd2_
+http://localhost:9000/API/ejercicio9/?id=6507549cedc519034cd40bd2
 
-**10.Listar losclientes conelDNIespecífico.**
+**10.Listar los clientes con el DNI específico.**
 
-
+_El DNI es solo un ejemplo, puede ser cualquier otro, los DNIs a utilizar estan en la carppeta `infobase_DB` especificamente en el archivo `tallerAutos.Clientes.json`_
+http://localhost:9000/API/ejercicio10/?dni=1234567890
 
 **11.Mostrar todos los automóviles con una capacidad mayor a 5 personas.**
 
+_El valor de capacidad puede ser cualquier núnero entero, si no se encuentra un valor que coincida, no arrojará resultados_
+http://localhost:9000/API/ejercicio11/?capacity=5
+
+**12.Obtener los detalles del alquiler que tiene fecha de inicio en '2023-07-05'.**
+
+_La fecha es solo un ejemplo, puede ser cualquier otra, los fechas a utilizar estan en la carppeta `infobase_DB` especificamente en el archivo `tallerAutos.Alquileres.json`, de lo contrario no arojará resultados_
+http://localhost:9000/API/ejercicio12/?date=2023-07-05
+
+**13.Listar las reservas pendientes realizadas por un cliente específico.**
+
+_El nombre y el apellido solo un ejemplo, puede ser cualquier otro, los nombres y apellidos a utilizar estan en la carppeta `infobase_DB` especificamente en el archivo `tallerAutos.Reservas.json` o `tallerAutos.Clientes.json`, de lo contrario no arojará resultados_
+http://localhost:9000/API/ejercicio13/?name=Luis&lstName=González
+
+**14.Mostrar los empleados con cargo de "Gerente" o "Asistente".**
+
+_Los cargos es solo un ejemplo, puede ser cualquier otro, los cargos a utilizar estan en la carppeta `infobase_DB` especificamente en el archivo `tallerAutos.Empleados.json`, de lo contrario no arojará resultados_
+http://localhost:9000/API/ejercicio14/?cargo=Gerente-Asistente
+
+**15.Obtener los datos de los clientes que realizaron al menos un alquiler.**
 
 
-**12.Obtenerlosdetallesdelalquilerquetienefechadeinicioen '2023-07-05'.**
+
+**16.Listar todos los automóviles ordenados por marca y modelo.**
 
 
 
-**13.Listarlasreservaspendientesrealizadasporuncliente específico.**
+**17.Mostrar la cantidad total de automóviles en cada sucursal junto con su dirección.**
 
-
-
-**14.Mostrarlosempleadosconcargode"Gerente"o"Asistente".**
-
-
-
-**15.Obtenerlosdatosdelosclientesquerealizaronalmenosun alquiler.**
-
-
-
-**16.Listartodoslosautomóvilesordenadospormarcaymodelo.**
-
-
-
-**17.Mostrarlacantidadtotaldeautomóvilesencadasucursaljunto consudirección.**
-
-
+http://localhost:9000/API/ejercicio17
 
 **18.Obtener la cantidad total de alquileres registrados en la base de datos.**
 
